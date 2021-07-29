dfadc5a048a30d4c2098002f0c2be58a4b9ee484
Steve Garon <steve.garon@cyber.gc.ca>
2021-07-29 15:39:42 +0000

    Allow services to be setup as well

--- setup_vscode.sh
+++ setup_vscode.sh
@@ -1,5 +1,19 @@
 #!/bin/sh -x
 
+usage() { echo "Usage: $0 [-h] [-s]" 1>&2; exit 1; }
+
+# Get command line options
+while getopts "sh" arg; do
+  case $arg in
+    s) # Setup services as well
+      services="yes"
+      ;;
+    h) # Show usage
+      usage
+      ;;
+  esac
+done
+
 # Prepare sysctl for VSCode
 sudo sysctl -w fs.inotify.max_user_watches=524288
 
@@ -14,7 +28,7 @@ then
     sudo apt-get install -y docker-ce docker-ce-cli containerd.io
 fi
 
-# Setup sudoless docker 
+# Setup sudoless docker
 sudo groupadd docker
 sudo usermod -aG docker $USER
 
@@ -27,10 +41,10 @@ then
 fi
 
 # Setup dependencies
-sudo apt-get update 
+sudo apt-get update
 sudo apt install -y software-properties-common
 sudo add-apt-repository -y ppa:deadsnakes/ppa
-sudo apt-get install -yy python3-venv python3.9 python3.9-dev python3.9-venv libffi7 
+sudo apt-get install -yy python3-venv python3.9 python3.9-dev python3.9-venv libffi7
 sudo apt-get install -yy libfuzzy2 libmagic1 libldap-2.4-2 libsasl2-2 build-essential libffi-dev libfuzzy-dev libldap2-dev libsasl2-dev libssl-dev
 
 # Allow connections to github.com via SSH
@@ -56,10 +70,73 @@ venv/bin/pip install -e ./assemblyline-service-client
 venv/bin/pip install -e ./assemblyline-ui
 venv/bin/pip install -e ./assemblyline-v4-service
 
-# Remove temporary created file during install 
+# Remove temporary created file during install
 rm -rf assemblyline-base/assemblyline/common/frequency.c
+
+if [ $services ]
+then
+  echo "Setting up services ..."
+
+  # Grab current directory
+  cwd=`cwd`
+
+  # Creating services directory
+  mkdir -p ../services
+  mv .vscode_services ../services/.vscode
+  cd ../services
+
+  # Setting up services venv
+  python3.9 -m venv venv
+  venv/bin/pip install -U pip
+  venv/bin/pip install -U wheel
+  venv/bin/pip install -U pytest pytest-cov fakeredis[lua] retrying codecov flake8 pep8 autopep8 ipython packaging gitpython faker requests_mock hachoir
+  venv/bin/pip install -e $cwd/assemblyline-base
+  venv/bin/pip install -e $cwd/assemblyline-core
+  venv/bin/pip install -e $cwd/assemblyline-service-client
+  venv/bin/pip install -e $cwd/assemblyline-v4-service
+
+  # Clone git repos
+  git clone git@github.com:CybercentreCanada/assemblyline-service-antivirus.git || git clone https://github.com/CybercentreCanada/assemblyline-service-antivirus.git
+  git clone git@github.com:CybercentreCanada/assemblyline-service-apkaye.git || git clone https://github.com/CybercentreCanada/assemblyline-service-apkaye.git
+  git clone git@github.com:CybercentreCanada/assemblyline-service-avclass.git || git clone https://github.com/CybercentreCanada/assemblyline-service-avclass.git
+  git clone git@github.com:CybercentreCanada/assemblyline-service-beaver.git || git clone https://github.com/CybercentreCanada/assemblyline-service-beaver.git
+  git clone git@github.com:CybercentreCanada/assemblyline-service-characterize.git || git clone https://github.com/CybercentreCanada/assemblyline-service-characterize.git
+  git clone git@github.com:CybercentreCanada/assemblyline-service-configextractor.git || git clone https://github.com/CybercentreCanada/assemblyline-service-configextractor.git
+  git clone git@github.com:CybercentreCanada/assemblyline-service-cuckoo.git || git clone https://github.com/CybercentreCanada/assemblyline-service-cuckoo.git
+  git clone git@github.com:CybercentreCanada/assemblyline-service-deobfuscripter.git || git clone https://github.com/CybercentreCanada/assemblyline-service-deobfuscripter.git
+  git clone git@github.com:CybercentreCanada/assemblyline-service-emlparser.git || git clone https://github.com/CybercentreCanada/assemblyline-service-emlparser.git
+  git clone git@github.com:CybercentreCanada/assemblyline-service-espresso.git || git clone https://github.com/CybercentreCanada/assemblyline-service-espresso.git
+  git clone git@github.com:CybercentreCanada/assemblyline-service-extract.git || git clone https://github.com/CybercentreCanada/assemblyline-service-extract.git
+  git clone git@github.com:CybercentreCanada/assemblyline-service-floss.git || git clone https://github.com/CybercentreCanada/assemblyline-service-floss.git
+  git clone git@github.com:CybercentreCanada/assemblyline-service-frankenstrings.git || git clone https://github.com/CybercentreCanada/assemblyline-service-frankenstrings.git
+  git clone git@github.com:CybercentreCanada/assemblyline-service-iparse.git || git clone https://github.com/CybercentreCanada/assemblyline-service-iparse.git
+  git clone git@github.com:CybercentreCanada/assemblyline-service-metadefender.git || git clone https://github.com/CybercentreCanada/assemblyline-service-metadefender.git
+  git clone git@github.com:CybercentreCanada/assemblyline-service-metapeek.git || git clone https://github.com/CybercentreCanada/assemblyline-service-metapeek.git
+  git clone git@github.com:CybercentreCanada/assemblyline-service-oletools.git || git clone https://github.com/CybercentreCanada/assemblyline-service-oletools.git
+  git clone git@github.com:CybercentreCanada/assemblyline-service-pdfid.git || git clone https://github.com/CybercentreCanada/assemblyline-service-pdfid.git
+  git clone git@github.com:CybercentreCanada/assemblyline-service-peepdf.git || git clone https://github.com/CybercentreCanada/assemblyline-service-peepdf.git
+  git clone git@github.com:CybercentreCanada/assemblyline-service-pefile.git || git clone https://github.com/CybercentreCanada/assemblyline-service-pefile.git
+  git clone git@github.com:CybercentreCanada/assemblyline-service-pixaxe.git || git clone https://github.com/CybercentreCanada/assemblyline-service-pixaxe.git
+  git clone git@github.com:CybercentreCanada/assemblyline-service-safelist.git || git clone https://github.com/CybercentreCanada/assemblyline-service-safelist.git
+  git clone git@github.com:CybercentreCanada/assemblyline-service-sigma.git || git clone https://github.com/CybercentreCanada/assemblyline-service-sigma.git
+  git clone git@github.com:CybercentreCanada/assemblyline-service-suricata.git || git clone https://github.com/CybercentreCanada/assemblyline-service-suricata.git
+  git clone git@github.com:CybercentreCanada/assemblyline-service-swiffer.git || git clone https://github.com/CybercentreCanada/assemblyline-service-swiffer.git
+  git clone git@github.com:CybercentreCanada/assemblyline-service-torrentslicer.git || git clone https://github.com/CybercentreCanada/assemblyline-service-torrentslicer.git
+  git clone git@github.com:CybercentreCanada/assemblyline-service-unpacker.git || git clone https://github.com/CybercentreCanada/assemblyline-service-unpacker.git
+  git clone git@github.com:CybercentreCanada/assemblyline-service-unpacme.git || git clone https://github.com/CybercentreCanada/assemblyline-service-unpacme.git
+  git clone git@github.com:CybercentreCanada/assemblyline-service-vipermonkey.git || git clone https://github.com/CybercentreCanada/assemblyline-service-vipermonkey.git
+  git clone git@github.com:CybercentreCanada/assemblyline-service-virustotal-dynamic.git || git clone https://github.com/CybercentreCanada/assemblyline-service-virustotal-dynamic.git
+  git clone git@github.com:CybercentreCanada/assemblyline-service-virustotal-static.git || git clone https://github.com/CybercentreCanada/assemblyline-service-virustotal-static.git
+  git clone git@github.com:CybercentreCanada/assemblyline-service-XLMMacroDeobfuscator.git || git clone https://github.com/CybercentreCanada/assemblyline-service-XLMMacroDeobfuscator.git
+  git clone git@github.com:CybercentreCanada/assemblyline-service-yara.git || git clone https://github.com/CybercentreCanada/assemblyline-service-yara.git
+
+  # Return to directory
+  cd $cwd
+fi
+
 
 # Self destruct!
 rm -rf .git*
 rm -rf setup_vscode.sh
 rm -rf README.md
+rm -rf .vscode_services
